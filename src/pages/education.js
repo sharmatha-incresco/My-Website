1e00299137b652165fb4eeb38d470b88d1c1e4b5
sharmatha-incresco <sharmatha.v@increscotech.com>
2023-09-14 18:37:52 +0530

    feat(education)-add education anf fix hover

--- src/pages/education.js
+++ src/pages/education.js
@@ -1,18 +1,123 @@
 import Footer from "./footer";
 import Topbar from "./topbar";
-
-
+import clg from "../clg.jpeg";
+import { MdLocationPin ,MdMenuBook} from "react-icons/md";
+import {BsCalendarWeekFill,BsBookmarkStarFill} from "react-icons/bs"
+import gg from "../gg.jpeg"
+import vellalar from "../vellalar.jpeg"
+import urc from "../urc.jpeg"
 function Education() {
+  function Institution({ name, img, year, std, location ,place,website,percentege}) {
+    return (
+      <div className="border-2 border-red-300 p-4 lg:w-[1024px] hover hover:bg-slate-200  ">
+        <div className="text-xl pb-3 font-semibold ">{name}</div>
+        <div className="flex md:flex-row lg:flex-row flex-col  gap-10">
+          <div>
+            <a href={website} className="cursor-pointer">
+            <img
+              className="avatar border-2 border-lime-400"
+              src={img}
+              width="500"
+              height="500"
+              style={{ height: '300px' ,width:'500px'}} 
+            />
+            </a>
+          </div>
+          <div className="flex-col flex justify-between">
+            <div className="flex">
+              <BsCalendarWeekFill className="w-5 h-5 text-red-300 "/>
+              <p className=" font-semibold text-red-300 text-base pl-1 pr-4">
+                Year :
+              </p>
+              {year}
+            </div>
+            <div className="flex pt-5 pb-5 ">
+              <MdMenuBook className="w-6 h-6 text-red-300"/>
+              <p className=" font-semibold text-red-300 text-base pl-1 pr-4">
+                Course:
+              </p>
+              {std}
+            </div>
+            <div className="flex  pb-5 ">
+              <BsBookmarkStarFill className="w-5 h-5 text-red-300"/>
+              <p className=" font-semibold text-red-300 text-base pl-1 pr-4">
+                
+                Percentege:
+              </p>
+              {percentege}
+            </div>
+            <a href={location}>
+              <div className="flex">
+                <MdLocationPin className="w-6 h-6 text-red-300" />
+                <p className="font-semibold text-red-300 text-base pr-4">
+                  Location:
+                </p>
+                {place}
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col min-h-screen">
-    <div>
-      <Topbar />
+      <div>
+        <Topbar />
+      </div>
+      <div className="flex-grow justify-center items-center p-4">
+        <div className="text-3xl text-red-300 underline font-bold pb-2">
+          EDUCATION
+        </div>
+        <div className="flex justify-center items-center ">
+        <div className="grid grid-cols-1 items-center justify-center p-10 gap-4 overflow-scroll">
+        <Institution
+          name="Velalar College Of Engineering & Technology"
+          img={clg}
+          year="2020-Till Now"
+          std="BE-CSE"
+          location="https://maps.app.goo.gl/TZ3A8tLxULhucNJK6?g_st=iw"
+          website="https://velalarengg.ac.in/"
+          place="Thindal,Erode"
+          percentege="90%"
+        />
+        <Institution
+          name="Green Graden Mr.Hr.Sec School "
+          img={gg}
+          year="2018-2020"
+          std="11th,12th"
+          website="http://www.greengardenschool.co.in/"
+          location="https://goo.gl/maps/WPmVBKqjC1NtZwmD6"
+          place="Perundurai R.S,Erode"
+          percentege="80.2%"
+        />
+         <Institution
+          name="Vellalar Girls Mr.Hr.Sec School "
+          img={vellalar}
+          year="2016-2018"
+          std="9th,10th"
+          website="https://schools.org.in/erode/33100704802/vellalar-high-school-for-girls-thindal.html"
+          location="https://goo.gl/maps/BF8W4zhSZWJ28Q9d8"
+          place="Thindal,Erode"
+          percentege="89%"
+        />
+        <Institution
+          name="URC Palaniammal Mr.Hr.Sec School"
+          img={urc}
+          year="2013-2016"
+          std="6th,7th,8th"
+          website="http://urcpmhss.edu.in/"
+          location="https://goo.gl/maps/96twi98NtPWPqMmX7"
+          place="Palayapalayam,Erode"
+          percentege="90%"
+        />
+      </div>
+      </div>
+      </div>
+      <div className="">
+        <Footer />
+      </div>
     </div>
-    <div className="flex-grow"></div>
-    <div className="">
-      <Footer />
-    </div>
-  </div>
   );
 }
 
--- src/pages/topbar.js
+++ src/pages/topbar.js
@@ -1,36 +1,34 @@
 import React from "react";
-import { HiHome ,HiAcademicCap,HiIdentification} from "react-icons/hi";
-import {RiGalleryFill} from "react-icons/ri"
-import { useNavigate } from "react-router-dom";
+import { HiHome, HiAcademicCap, HiIdentification } from "react-icons/hi";
+import { RiGalleryFill } from "react-icons/ri";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function Topbar() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const buttonClass = (path) =>
+    location.pathname === path
+      ? "bg-red-100 text-black"
+      : "bg-transparent text-black";
 
   return (
-    <div className="flex gap-4 justify-between p-4  bg-red-300">
-      <button onClick={() => navigate("/home")}>
-        <div className="flex gap-2">
-          <HiHome className="w-6 h-6" />
-          <p className="hidden md:block lg:block">Home</p>
-        </div>
+    <div className="flex gap-4 justify-between p-4 bg-red-300">
+      <button onClick={() => navigate("/home")} className={`flex gap-2 hover hover:bg-red-100 rounded-lg ${buttonClass("/home")}`}>
+        <HiHome className="w-6 h-6" />
+        <p className="hidden md:block lg:block">Home</p>
       </button>
-      <button onClick={() => navigate("/education")}>
-        <div className="flex gap-2">
-          <HiAcademicCap className="w-6 h-6" />
-          <p className="hidden md:block lg:block">Education</p>
-        </div>
+      <button onClick={() => navigate("/education")} className={`flex gap-2 hover hover:bg-red-100 rounded-lg ${buttonClass("/education")}`}>
+        <HiAcademicCap className="w-6 h-6" />
+        <p className="hidden md:block lg:block">Education</p>
       </button>
-      <button onClick={() => navigate("/experience")}>
-        <div className="flex gap-2">
-          <HiIdentification className="w-6 h-6" />
-          <p className="hidden md:block lg:block">Experience</p>
-        </div>
+      <button onClick={() => navigate("/experience")} className={`flex gap-2 hover hover:bg-red-100 rounded-lg ${buttonClass("/experience")}`}>
+        <HiIdentification className="w-6 h-6" />
+        <p className="hidden md:block lg:block">Experience</p>
       </button>
-      <button onClick={() => navigate("/certificategallery")}>
-        <div className="flex gap-2">
-          <RiGalleryFill className="w-6 h-6" />
-          <p className="hidden md:block lg:block">Certificate Gallery</p>
-        </div>
+      <button onClick={() => navigate("/certificategallery")} className={`flex gap-2 hover hover:bg-red-100 rounded-lg ${buttonClass("/certificategallery")}`}>
+        <RiGalleryFill className="w-6 h-6" />
+        <p className="hidden md:block lg:block">Certificate Gallery</p>
       </button>
     </div>
   );
