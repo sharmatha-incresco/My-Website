b243abca3b229fec96d98abf8e83682a04f1bc1c
sharmatha-incresco <sharmatha.v@increscotech.com>
2023-09-14 11:06:24 +0530

    sample

--- src/index.js
+++ src/index.js
@@ -3,34 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Applyform from './component/applyform';
+
 import './i18n';
-import Screen from './screen';
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import Apply from './component/applyform';
-import * as Sentry from "@sentry/react";
-import axios from 'axios';
-import { changeLanguage } from 'i18next';
-import SignUp from './component/signup';
-changeLanguage('ta')
-Sentry.init({
-  dsn: "https://d037bc4523fb4e61a1507f54ee6cd7f7@o4505514404806656.ingest.sentry.io/4505514481745920",
-  integrations: [
-    new Sentry.BrowserTracing({
-      // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-      tracePropagationTargets: ["localhost", "https:yourserver.io/api/"],
-    }),
-    new Sentry.Replay(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0,
-  // Session Replay
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-});
+
 
 
 const router = createBrowserRouter([
@@ -38,19 +18,7 @@ const router = createBrowserRouter([
     path: "/",
     element: <App />,
   },
-  {
-    path: "/applyform",
-    element: <Applyform />,
-  },
-  {
-    path: "/screen",
-    element: <Screen />,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
